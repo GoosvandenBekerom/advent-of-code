f13a8d6abe488b78ddd4f1d75867f06cebf26f92
gvandenbekerom <gvandenbekerom@bol.com>
2021-12-01 18:21:10 +0100

    add 2021 day 1

--- 2021/day01/main.go
+++ 2021/day01/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "embed"
+	"strconv"
 
 	"fmt"
 	"log"
@@ -23,14 +24,41 @@ func main() {
 func part1() {
 	fmt.Println("\n___________________________________________")
 	fmt.Println("part 1:")
+
+	count := -1
+	var previous int
 	for _, line := range strings.Split(input, "\n") {
-		println(line)
+		current := toInt(line)
+		if current > previous {
+			count++
+		}
+		previous = current
 	}
+	fmt.Printf("increased %d times\n", count)
 }
 
 func part2() {
 	fmt.Println("\n___________________________________________")
 	fmt.Println("part 2:")
+
+	lines := strings.Split(input, "\n")
+	measurements := make([]int, len(lines))
+
+	for i, line := range lines {
+		measurements[i] = toInt(line)
+	}
+
+	count := -1
+	var previous int
+	for i := 1; i < len(measurements)-1; i++ {
+		current := measurements[i-1] + measurements[i] + measurements[i+1]
+		if current > previous {
+			count++
+		}
+		previous = current
+	}
+
+	fmt.Printf("increased %d times\n", count)
 }
 
 // ----------------------------------------
@@ -41,4 +69,10 @@ func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func toInt(s string) int {
+	v, err := strconv.Atoi(s)
+	check(err)
+	return v
 }
